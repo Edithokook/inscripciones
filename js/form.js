f214353ce15f6e7ff61f4d6508e58d24f628d8ae
Edithokook <90534049+Edithokook@users.noreply.github.com>
2021-09-21 18:07:32 -0500

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,46 @@
+document.querySelector("#submit").addEventListener("click", e => {
+  e.preventDefault();
+
+  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  let telefono = "51973180997";
+  let servicio = document.querySelector("#servicio").value;
+  let cliente = document.querySelector("#cliente").value;
+  let dni = document.querySelector("#dni").value;
+  let correo = document.querySelector("#correo").value;
+  let celular = document.querySelector("#celular").value;
+  let dirección = document.querySelector("#dirección").value;
+  let comprobante = document.querySelector("#comprobante").value;
+  let resp = document.querySelector("#respuesta");
+
+  resp.classList.remove("fail");
+  resp.classList.remove("send");
+
+  let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_LP DERECHO_*%0A
+		*Ficha de inscripción*%0A%0A
+		*Nombre del curso o diplomado:*%0A
+		${servicio}%0A
+		*Nombres completos:*%0A
+		${cliente}%0A
+		*DNI:*%0A
+		${dni}%0A
+		*Correo electrónico:*%0A
+		${correo}%0A
+    *Celular:*%0A
+		${celular}%0A
+    *Dirección detallada:*%0A
+		${dirección}%0A
+    *Comprobante de pago:*%0A
+		${comprobante}`;
+
+  if (servicio === "" || cliente === "" || correo === ""|| celular === ""|| dirección === ""|| comprobante === "") {
+    resp.classList.add("fail");
+    resp.innerHTML = `Faltan algunos datos.`;
+    return false;
+  }
+  resp.classList.remove("fail");
+  resp.classList.add("send");
+  resp.innerHTML = `Se ha enviado tu ficha de inscripción.`;
+
+  window.open(url);
+});
