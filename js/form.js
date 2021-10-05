c4c19887002147e7ada46d0a9c913ba8bd0fd1e7
Edithokook <90534049+Edithokook@users.noreply.github.com>
2021-10-04 21:05:43 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -16,22 +16,22 @@ document.querySelector("#submit").addEventListener("click", e => {
   resp.classList.remove("send");
 
   let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
-		*_LP DERECHO_*%0A
-		*Ficha de inscripción*%0A%0A
-		*Nombre del curso o diplomado:*%0A
-		${servicio}%0A
-		*Nombres completos:*%0A
-		${cliente}%0A
-		*DNI:*%0A
-		${dni}%0A
-		*Correo electrónico:*%0A
-		${correo}%0A
-    *Celular:*%0A
-		${celular}%0A
-    *Dirección detallada:*%0A
-		${dirección}%0A
-    *Comprobante de pago:*%0A
-		${comprobante}`;
+ *_LP DERECHO_*%0A
+	*Ficha de inscripción*%0A%0A
+	*Nombre del curso o diplomado:*%0A
+	${servicio}%0A
+	*Nombres completos:*%0A
+	${cliente}%0A
+	*DNI:*%0A
+	${dni}%0A
+	*Correo electrónico:*%0A
+	${correo}%0A
+	*Celular:*%0A
+	${celular}%0A
+	*Dirección detallada:*%0A
+	${dirección}%0A
+	*Comprobante de pago:*%0A
+	${comprobante}%0A
 
   if (servicio === "" || cliente === "" || correo === ""|| celular === ""|| dirección === ""|| comprobante === "") {
     resp.classList.add("fail");
